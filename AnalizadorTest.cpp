fadc165accc3c7788ef45d6dad99ac328df4a600
Dani C R <danielcr86@gmail.com>
2016-09-21 00:21:53 +0100

    Tests for Analizador::agruparPorTematica

--- AnalizadorTest.cpp
+++ AnalizadorTest.cpp
@@ -1,3 +1,4 @@
+
 #include <gmock/gmock.h>
 
 #include "Analizador.h"
@@ -155,4 +156,192 @@ TEST(AnalizadorTest, givenAnalizadorWithThreeNoticiasTwoAndThreeAreAgrupables_wh
    ASSERT_THAT(groups, UnorderedElementsAre( UnorderedElementsAre ( NoticiaIfPtr(noticia2), NoticiaIfPtr(noticia3) ),
                                              std::list<NoticiaIfPtr>({NoticiaIfPtr(noticia1)})
                                            ));
+}
+
+TEST(AnalizadorTest, givenEmptyGrouping_whenCallingAgrupar_thenReturnsEmptyGroup) {
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({});
+
+   ASSERT_THAT(groups, IsEmpty());
+}
+
+TEST(AnalizadorTest, givenOneGroupingWithOneNoticia_whenCallingAgrupar_thenReturnsOneGroup) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica(
+      {std::list<NoticiaIfPtr>({NoticiaIfPtr(noticia1)})}
+   );
+
+   ASSERT_THAT(groups, UnorderedElementsAre(std::list<NoticiaIfPtr>({NoticiaIfPtr(noticia1)})));
+}
+
+TEST(AnalizadorTest, givenThreeGroupsOfNoticiasNotGroupable_whenCallingAgrupar_thenReturnsThreeGroups) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia2(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia3(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia4(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia5(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia6(new NoticiaIfMock());
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia3, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia4, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia6, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6)} )
+   });
+
+   ASSERT_THAT(groups, UnorderedElementsAre(
+      UnorderedElementsAre(NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)),
+      UnorderedElementsAre(NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)),
+      UnorderedElementsAre(NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6))
+   ));
+}
+
+TEST(AnalizadorTest, givenThreeGroupsOfNoticiasAllGroupable_whenCallingAgrupar_thenReturnsOneGroup) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia2(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia3(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia4(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia5(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia6(new NoticiaIfMock());
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia3, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia4, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia6, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(Ref(*noticia3))).Times(1).WillOnce(Return(true));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(Ref(*noticia6))).Times(1).WillOnce(Return(true));
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6)} )
+   });
+
+   ASSERT_THAT(groups, UnorderedElementsAre( UnorderedElementsAre (
+      NoticiaIfPtr(noticia1),
+      NoticiaIfPtr(noticia2),
+      NoticiaIfPtr(noticia3),
+      NoticiaIfPtr(noticia4),
+      NoticiaIfPtr(noticia5),
+      NoticiaIfPtr(noticia6)
+   )));
+}
+
+TEST(AnalizadorTest, givenThreeGroupsOfNoficiasOneAndTwoGroupable_whenCallingAgrupar_thenReturnsTwoGroups) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia2(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia3(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia4(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia5(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia6(new NoticiaIfMock());
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia3, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia4, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia6, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(Ref(*noticia3))).Times(1).WillOnce(Return(true));
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6)} )
+   });
+
+   ASSERT_THAT(groups, UnorderedElementsAre(
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia1),
+         NoticiaIfPtr(noticia2),
+         NoticiaIfPtr(noticia3),
+         NoticiaIfPtr(noticia4)),
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia5),
+         NoticiaIfPtr(noticia6))
+   ));
+}
+
+TEST(AnalizadorTest, givenThreeGroupsOfNoticiasOneAndThreeGroupable_whenCallingAgrupar_thenReturnsTwoGroups) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia2(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia3(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia4(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia5(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia6(new NoticiaIfMock());
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia3, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia4, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia6, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(Ref(*noticia5))).Times(1).WillOnce(Return(true));
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6)} )
+   });
+
+   ASSERT_THAT(groups, UnorderedElementsAre(
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia1),
+         NoticiaIfPtr(noticia2),
+         NoticiaIfPtr(noticia5),
+         NoticiaIfPtr(noticia6)),
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia4),
+         NoticiaIfPtr(noticia3))
+   ));
+}
+
+TEST(AnalizadorTest, givenThreeGroupsOfNoticiasTwoAndThreeGroupable_whenCallingAgrupar_thenReturnsTwoGroups) {
+   std::shared_ptr<NoticiaIfMock> noticia1(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia2(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia3(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia4(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia5(new NoticiaIfMock());
+   std::shared_ptr<NoticiaIfMock> noticia6(new NoticiaIfMock());
+   EXPECT_CALL(*noticia1, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia2, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia3, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia4, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia6, esAgrupablePorTematica(_)).WillRepeatedly(Return(false));
+   EXPECT_CALL(*noticia5, esAgrupablePorTematica(Ref(*noticia3))).Times(1).WillOnce(Return(true));
+
+   Analizador a;
+
+   std::list<std::list<NoticiaIfPtr> > groups = a.agruparNoticiasPorTematica({
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia1), NoticiaIfPtr(noticia2)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia3), NoticiaIfPtr(noticia4)} ),
+      std::list<NoticiaIfPtr>( {NoticiaIfPtr(noticia5), NoticiaIfPtr(noticia6)} )
+   });
+
+   ASSERT_THAT(groups, UnorderedElementsAre(
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia3),
+         NoticiaIfPtr(noticia4),
+         NoticiaIfPtr(noticia5),
+         NoticiaIfPtr(noticia6)),
+      UnorderedElementsAre (
+         NoticiaIfPtr(noticia1),
+         NoticiaIfPtr(noticia2))
+   ));
 }
