45936ef4515501ccb5beddf11c090938a6cc0fb0
Dani C R <danielcr86@gmail.com>
2016-09-20 21:06:47 +0100

    Show theme grouping in Main

--- Main.cpp
+++ Main.cpp
@@ -18,7 +18,8 @@ int main() {
 
 	std::list<std::list<NoticiaIfPtr> > groups = a1.agruparNoticiasPorEntidadMasFrecuente();
 
-
+	std::cout << "POR ENTIDAD MAS NOMBRADA:" << std::endl;
+	std::cout << std::endl;
 	for (std::list<NoticiaIfPtr> grupo : groups) {
 		std::cout << grupo.front()->getMasFrecuente().getEntidadNombrada() << std::endl;
 		for (NoticiaIfPtr noticia : grupo) {
@@ -27,6 +28,18 @@ int main() {
 		std::cout << "======" << std::endl;
 		std::cout << std::endl;
 	}
+
+	groups = a1.agruparNoticiasPorTematica(groups);
+
+        std::cout << "POR TEMA:" << std::endl;
+        std::cout << std::endl;
+        for (std::list<NoticiaIfPtr> grupo : groups) {
+                for (NoticiaIfPtr noticia : grupo) {
+                        std::cout << "*[" << noticia->getTitulo() << "]" << std::endl;
+                }
+                std::cout << "======" << std::endl;
+                std::cout << std::endl;
+        }
 
 	return 0;
 }
