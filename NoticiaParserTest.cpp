b36c5754cb125fc32dc0ad01a7e2afd9b990800b
Dani C R <danielcr86@gmail.com>
2016-09-26 21:12:31 +0100

    TDD: tests for noticia parser that not compile

---
+++ NoticiaParserTest.cpp
@@ -0,0 +1,47 @@
+/*
+ * NoticiaParserTest.cpp
+ *
+ *  Created on: 26 de sept. de 2016
+ *      Author: dancre
+ */
+
+#include <gmock/gmock.h>
+
+#include "Noticia.h"
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <stdio.h>
+
+using testing::StrEq;
+
+class NoticiaParserTest : public ::testing::Test {
+public:
+   static const std::string STOP_LIST_FILENAME;
+protected:
+   static void SetUpTestCase() {
+      std::ofstream stopList(STOP_LIST_FILENAME);
+      stopList << "EntidadExcluida" << std::endl;
+   }
+
+   static void TearDownTestCase() {
+      remove(STOP_LIST_FILENAME.c_str());
+   }
+};
+const std::string NoticiaParserTest::STOP_LIST_FILENAME = "stopList.txt";
+
+TEST_F(NoticiaParserTest, givenParser_whenCallingParse_thenReturnsNoticiaInstance) {
+	std::string noticia("Esto es el titulo de la noticia\n \
+			             Esto es el primer parrafo del cuerpo de la noticia.\n \
+			             Esto es la segunda frase del cuerpo de la noticia");
+
+	NoticiaParser parser(std::sstream(noticia), "stopList.txt");
+
+	Noticia parsedNoticia = parser.parse();
+
+	ASSERT_THAT(parsedNoticia.getTitulo(), StrEq("Esto es el titulo de la noticia"));
+	ASSERT_THAT(parsedNoticia.getCuerpo(), StrEq("Esto es el primer parrafo del cuerpo de la noticia.\n \
+			             Esto es la segunda frase del cuerpo de la noticia"));
+	ASSERT_THAT(parsedNoticia.getPalabrasReservadas(), ElementsAre("EntidadExcluida"));
+}
