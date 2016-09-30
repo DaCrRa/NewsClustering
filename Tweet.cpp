2dc5f3459edd64b4b2f58d68a1b281d009807c1a
Dani C R <danielcr86@gmail.com>
2016-09-30 01:39:22 +0100

    Try to fix tests:
    [  FAILED  ] 2 tests, listed below:
    [  FAILED  ] TweetTest.givenTweet_WhenCallingEsAgrupablePorTemaWithItemSharingEntity_thenReturnTrue
    [  FAILED  ] TweetGrouping.givenTwoTweetsSharingEntidad_whenCallingPorTematicaSonAgrupables_returnTrue
    
    Discovered that Tweet::agrupablePorTema is not complete, because
    if it is called with a tweet, it must return true if they share any
    entity. So we need add logic: function will return true also if item
    given shares any entity
    
    This fixes the 2 tests mentioned, but breaks this one:
    [  FAILED  ] 1 test, listed below:
    [  FAILED  ] NoticiaTweetGrouping.givenNoticiaAndTweetWithEntidadNoMasNombrada_whenCallingPorTematicaSonAgrupables_returnFalse
    
    We are breaking condition of Noticia-Tweet grouping, cause
    they shall be grouped only if tweet has most frequent entity...

--- Tweet.cpp
+++ Tweet.cpp
@@ -15,13 +15,21 @@ std::string Tweet::getTextoDestacado() const {
 
 bool Tweet::esAgrupablePorTematica(ItemAgrupable& n) const {
 	try {
-	EntidadNombrada masFrecuente = n.getMasFrecuente();
-	return std::find_if(entidades.begin(),
-			entidades.end(),
-			[&](const EntidadNombrada& e) -> bool {
-				return e.getEntidadNombrada().compare(masFrecuente.getEntidadNombrada()) == 0;
-			}
-	) != entidades.end();
+		EntidadNombrada masFrecuente = n.getMasFrecuente();
+		std::list<EntidadNombrada> nEntidades = n.getEntidades();
+		return std::find_if(entidades.begin(),
+				entidades.end(),
+				[&](const EntidadNombrada& e) -> bool {
+					return e.getEntidadNombrada().compare(masFrecuente.getEntidadNombrada()) == 0;
+				}
+		) != entidades.end() || std::find_if(entidades.begin(),
+				entidades.end(),
+				[&](const EntidadNombrada& e1) -> bool {
+					return std::find_if(nEntidades.begin(), nEntidades.end(), [&](const EntidadNombrada& e2) -> bool {
+						return e1.getEntidadNombrada().compare(e2.getEntidadNombrada()) == 0;
+					}) != nEntidades.end();
+				}
+		) != entidades.end();
 	} catch(NoEntidadNombradaException& e) {
 		return false;
 	}
