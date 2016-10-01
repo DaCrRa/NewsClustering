e6aa3a008f36e94d961c87606d4d10f464f825cb
Dani C R <danielcr86@gmail.com>
2016-10-01 21:32:42 +0100

    Compile and link NoticiaParserTest

---
+++ NoticiaJsonParser.cpp
@@ -0,0 +1,13 @@
+/*
+ * NoticiaJsonParser.cpp
+ *
+ *  Created on: 1 Oct 2016
+ *      Author: dancre
+ */
+
+#include "NoticiaJsonParser.h"
+
+Noticia NoticiaJsonParser::parse() {
+	return Noticia("", "", "");
+}
+
