dec6f9eccc6a1e7dfd5901a5117bcf21ad844d61
Dani C R <danielcr86@gmail.com>
2016-10-02 11:29:33 +0100

    TDD: GroupingJsonSerializer
    Created first test that does not compile

---
+++ GroupingJsonSerializerTest.cpp
@@ -0,0 +1,26 @@
+/*
+ * GroupingSerializerTest.cpp
+ *
+ *  Created on: 2 Oct 2016
+ *      Author: dancre
+ */
+
+#include <gmock/gmock.h>
+
+#include <sstream>
+
+#include "ItemAgrupableMock.h"
+#include "JsonParser.h"
+
+TEST(GroupingJsonSerializerTest, givenEmptyGrouping_whenCallingSerialize_thenReturnsEmptyJsonArray) {
+	std::list<std::list<ItemAgrupablePtr> > grouping;
+	std::ostringstream serializerOutputStream;
+
+	GroupingJsonSerializer serializer;
+	serializer.serialize(grouping, serializerOutputStream);
+
+	std::istringstream inputForParser(serializerOutputStream.str());
+	JsonParser p(inputForParser);
+	Json::Value parsedGrouping = p.parse();
+	ASSERT_TRUE(parsedGrouping.empty());
+}
