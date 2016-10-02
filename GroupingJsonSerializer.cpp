56eabfa0faba3caa7c89d4f5b6bfd510e8f377db
Dani C R <danielcr86@gmail.com>
2016-10-02 14:59:53 +0100

    Fixed test for GrouipingJsonSerializer

--- GroupingJsonSerializer.cpp
+++ GroupingJsonSerializer.cpp
@@ -7,6 +7,17 @@
 
 #include "GroupingJsonSerializer.h"
 
+#include <json/json.h>
+
 void GroupingJsonSerializer::serialize(const std::list<std::list<ItemAgrupablePtr> >& grouping, std::ostream& output) {
-	output << "[]";
+	Json::Value groupsJson;
+	int i = 0;
+	for (auto& group : grouping) {
+		int j = 0;
+		for (auto& item : group) {
+			groupsJson[i][j++] = item->getTextoDestacado();
+		}
+		i++;
+	}
+	output << groupsJson;
 }
